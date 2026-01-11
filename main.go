be25240ea12ccbc2b46f69ac651282f89fda5559
Yimeng Yu <yimeng.yu@gmail.com>
2026-01-11 10:10:51 -0800

    code for RESTful web service

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+//import the packages you’ll need to support the code you’ve just written.
+import (
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+)
+
+// album represents data about a record album.
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+//assign the handler function to an endpoint path.
+//This sets up an association in which getAlbums handles requests to the /albums endpoint path.
+// func main() {
+//     router := gin.Default()
+//     router.GET("/albums", getAlbums)
+
+//     router.Run("localhost:8080")
+// }
+
+// Change main function so that it includes the router.POST function, as in the following.
+// Associate the POST method at the /albums path with the postAlbums function.
+// func main() {
+//     router := gin.Default()
+//     router.GET("/albums", getAlbums)
+//     router.POST("/albums", postAlbums)
+
+//     router.Run("localhost:8080")
+// }
+
+//change your main so that it includes a new call to router.GET,
+// where the path is now /albums/:id, as shown in the following example.
+func main() {
+    router := gin.Default()
+    router.GET("/albums", getAlbums)
+    router.GET("/albums/:id", getAlbumByID)
+    router.POST("/albums", postAlbums)
+
+    router.Run("localhost:8080")
+}
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in the request body.
+
+//Use Context.BindJSON to bind the request body to newAlbum.
+//Append the album struct initialized from the JSON to the albums slice.
+//Add a 201 status code to the response, along with JSON representing the album you added.
+func postAlbums(c *gin.Context) {
+    var newAlbum album
+
+    // Call BindJSON to bind the received JSON to
+    // newAlbum.
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
+
+    // Add the new album to the slice.
+    albums = append(albums, newAlbum)
+    c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+//Use Context.Param to retrieve the id path parameter from the URL. When you map this handler to a path, you’ll include a placeholder for the parameter in the path.
+//Loop over the album structs in the slice, looking for one whose ID field value matches the id parameter value. If it’s found, you serialize that album struct to JSON and return it as a response with a 200 OK HTTP code.
+//As mentioned above, a real-world service would likely use a database query to perform this lookup.
+
+//Return an HTTP 404 error with http.StatusNotFound if the album isn’t found.
+func getAlbumByID(c *gin.Context) {
+    id := c.Param("id")
+
+    // Loop over the list of albums, looking for
+    // an album whose ID value matches the parameter.
+    for _, a := range albums {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
